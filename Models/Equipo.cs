b83aa862595b8e40f9938f4c0ce9a92d01eee613
David Aceves <deividvengeance@gmail.com>
2018-03-24 11:48:32 -0600

    Initial commit

---
+++ Models/Equipo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mate2.Models
+{
+    public class Equipo
+    {
+        public string EquipoId { get; set; }
+        public int EscuelaId { get; set; }
+        public string Contrasena { get; set; }
+        public int Mesa { get; set; }
+        public int Clave { get; set; }
+        public int Usuario { get; set; }
+    }
+}
